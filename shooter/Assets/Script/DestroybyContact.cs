9013174142970560ddcc204569d14f07ffc2ccf6
ErronStone <wyatta3998@gmail.com>
2019-11-17 23:17:47 -0500

    Challenge-3

---
+++ shooter/Assets/Script/DestroybyContact.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroybyContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public int scoreValue;
+    public GameObject playerExplosion;
+    private GameController gameController;
+
+    private void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary" || other.tag == "Enemy")
+        {
+            return;
+        }
+
+        if (explosion != null)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+        }
+        gameController.AddScore (scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
